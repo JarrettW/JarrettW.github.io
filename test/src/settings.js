ca2559bef4c79c2c3438a47c894cc93bc0a14a8b
JarrettW <jarrett_w@outlook.com>
2018-03-19 11:09:59 +0800

    test dmsg

---
+++ test/src/settings.js
@@ -0,0 +1,62 @@
+_CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {},
+        internal: {
+            "71VhFCTINJM6/Ky3oX9nBT": [
+                "image/default_btn_disabled.png",
+                0
+            ],
+            "e8Ueib+qJEhL6mXAHdnwbi": [
+                "image/default_btn_normal.png",
+                0
+            ],
+            "b4P/PCArtIdIH38t6mlw8Y": [
+                "image/default_btn_pressed.png",
+                0
+            ],
+            "02delMVqdBD70a/HSD99FK": [
+                "image/default_sprite_splash.png",
+                0
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.Texture2D"
+    ],
+    launchScene: "db://assets/scenes/New Scene.fire",
+    scenes: [
+        {
+            url: "db://assets/scenes/New Scene.fire",
+            uuid: 0
+        }
+    ],
+    packedAssets: {
+        "0be8eac0b": [
+            "29FYIk+N1GYaeWH/q1NxQO",
+            0,
+            "a2MjXRFdtLlYQ5ouAFv/+R",
+            "e97GVMl6JHh5Ml5qEDdSGa",
+            "f0BIwQ8D5Ml7nTNQbh1YlS"
+        ]
+    },
+    orientation: "portrait",
+    uuids: [
+        "3di6FDMupO2YVGmZ/UqoS5"
+    ],
+    "md5AssetsMap": {
+        "0b/0be8eac0b.json": "01982",
+        "internal/image/default_btn_disabled.png": "286c6",
+        "internal/image/default_btn_normal.png": "1ecb7",
+        "internal/image/default_btn_pressed.png": "bedf4",
+        "internal/image/default_sprite_splash.png": "cea68"
+    }
+}
